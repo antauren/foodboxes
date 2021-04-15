6dad59a928c246c2729a221427bfba15f61b60f9
Anatoly <antauren@yandex.ru>
2021-04-15 21:04:21 +0300

    add model Reviews

--- foodboxes/reviews/models.py
+++ foodboxes/reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Reviews(models.Model):
+    PUBLISHED = 'published'
+    ON_MODERATION = 'on moderation'
+    REJECTED = 'rejected'
+
+    STATUS_CHOICES = (
+        (PUBLISHED, 'Опубликован'),
+        (ON_MODERATION, 'На модерации'),
+        (REJECTED, 'Отклонен'),
+    )
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField('Отзыв')
+    created_at = models.DateTimeField('Дата создания')
+    published_at = models.DateTimeField('Дата публикации')
+    status = models.CharField('Статус', choices=STATUS_CHOICES, default=ON_MODERATION, max_length=20)
