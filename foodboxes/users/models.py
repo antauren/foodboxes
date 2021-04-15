8f3853dae3927e6dcc78493894be2dcb19b9db24
Anatoly <antauren@yandex.ru>
2021-04-15 19:42:46 +0300

    add model User

--- foodboxes/users/models.py
+++ foodboxes/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    middle_name = models.CharField('Отчество')
+    phone = models.CharField('Телефон')
+    address = models.CharField('Адрес')
