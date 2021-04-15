a1c686b93df14cac035bcf2e71e1d798df429ace
Anatoly <antauren@yandex.ru>
2021-04-15 19:37:31 +0300

    add model Item

--- foodboxes/items/models.py
+++ foodboxes/items/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField('Наименование')
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение')
+    weight = models.PositiveSmallIntegerField('Вес в граммах')
+    price = models.DecimalField('Цена в рублях')
