09f337d14a6c3e527ca3e1e9a586cad760ea7b08
Anatoly <antauren@yandex.ru>
2021-04-16 16:31:58 +0300

    add function-based view

--- foodboxes/items/views.py
+++ foodboxes/items/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Item
+
+
+def get_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+
+    response_dict = {
+        'id': item.id,
+        'title': item.title,
+        'description': item.description,
+        'image': item.image.url,
+        'weight': item.weight,
+        'price': item.price,
+
+    }
+
+    return JsonResponse(response_dict, safe=False, json_dumps_params={'indent': 4, 'ensure_ascii': False})
