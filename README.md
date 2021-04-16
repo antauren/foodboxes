201f356c6164ae2a577f910c6e455cdff09d60e4
Anatoly <antauren@yandex.ru>
2021-04-16 10:52:17 +0300

    add management command 'load_users'

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # foodboxes
+
+## Загрузить тестовые данные
+
+```
+python manage.py load_users -u https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/recipients.json
+```
---
+++ foodboxes/users/management/commands/load_users.py
@@ -0,0 +1,47 @@
+import requests
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('-u', '--url', required=True)
+
+    def handle(self, *args, **options):
+        url = options['url']
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        users = response.json()
+
+        for user_dict in users:
+            id_ = user_dict['id']
+
+            email = user_dict['email']
+            password = user_dict['password']
+            info = user_dict['info']
+            contacts = user_dict['contacts']
+            city_kladr = user_dict['city_kladr']
+            premium = user_dict['premium']
+
+            surname = info['surname']
+            name = info['name']
+            patronymic = info['patronymic']
+
+            phoneNumber = contacts['phoneNumber']
+            username = email.split('@')[0]
+
+            user, _ = User.objects.update_or_create(id=id_,
+                                                    defaults={'password': password,
+                                                              'phone': phoneNumber,
+                                                              'middle_name': patronymic,
+                                                              'username': username,
+                                                              'address': city_kladr,
+                                                              'email': email,
+                                                              'first_name': name,
+                                                              'last_name': surname,
+                                                              },
+                                                    )
